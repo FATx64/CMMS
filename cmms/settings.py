6bc212372d2bf359a8b2109d857063cbdc729f55
ziro <palembani@gmail.com>
2023-05-07 16:34:14 +0700

    feat: Initial template

--- cmms/settings.py
+++ cmms/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -49,12 +48,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cmms.core.urls'
+ROOT_URLCONF = 'cmms.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cmms.core.wsgi.application'
+WSGI_APPLICATION = 'cmms.wsgi.application'
 
 
 # Database
--- cmms/urls.py
+++ cmms/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from cmms.views import HomeView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", HomeView.as_view()),
 ]
