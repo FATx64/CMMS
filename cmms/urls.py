c6f9b3c9f669bdf3ef8239ad5031d19913bfda1e
ziro <palembani@gmail.com>
2023-05-08 08:39:58 +0700

    feat: Tailwand + `base.html`

--- cmms/urls.py
+++ cmms/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import include, path
 
 from cmms.views import HomeView
 
 urlpatterns = [
     path("", HomeView.as_view()),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
