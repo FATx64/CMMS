5e54b472a256aa3d3ae0c5dc93e53f5029dccfc3
ziro <palembani@gmail.com>
2023-05-23 07:15:26 +0700

    feat: Add scripts to poetry for faster deploy REF: https://github.com/kanu/django-poetry-example/blob/978dae36ab88fb83aa4ffd1e015213692e107e24/manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+
+from cmms.manage import main as _main
+from scripts import MODULE_NAME
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmms.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    root = os.path.join(os.getcwd(), MODULE_NAME)
+    sys.path.insert(0, root)
+    os.chdir(root)
+
+    _main()  # django's default manage.py
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
