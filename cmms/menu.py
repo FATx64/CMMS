21e74cc58ced450d18caa2a8fd5e0653e94a0e5e
ziro <palembani@gmail.com>
2023-05-25 07:44:48 +0700

    chore: Improve menu system * Added submenu (childrens) support * Not yet constructed items now raise ItemNotConstructed instead of   generic RuntimeError

--- cmms/menu.py
+++ cmms/menu.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from __future__ import annotations
+
 from django.conf import settings
 
 
+class ItemNotConstructed(RuntimeError):
+    def __init__(self):
+        super().__init__("Item is not yet constructed!")
+
 class Item:
-    def __init__(self, label: str = None, url: str = None, icon: str = None):
+    def __init__(self, label: str = None, url: str = None, icon: str = None, *, children: list[Item] = []):
         self.label = label
         self.url = url
         # Use Google "Symbols" name. Not "Icons", to avoid future potential breakage
         self.icon = icon or "broken_image"
         self.context = None
         self.request = None
+        self.children = children
 
     @property
     def constructed(self):
@@ -22,7 +28,7 @@ class Item:
 
     def is_active(self) -> bool:
         if not self.constructed:
-            raise RuntimeError
+            raise ItemNotConstructed
         return self.request.META["PATH_INFO"].rstrip("/") == self.url  # type: ignore
 
 
