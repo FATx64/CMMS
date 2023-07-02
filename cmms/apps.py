81847d0906f45e63b21db65586e4e6b8041720e5
Ahmad Ansori Palembani <palembani@gmail.com>
2023-07-02 11:36:20 +0700

    fix: The check failed on macOS for some reason?

--- cmms/apps.py
+++ cmms/apps.py
@@ -7,6 +7,9 @@ class CMMSConfig(AppConfig):
     name = "cmms"
 
     def ready(self):
+        if "manage.py" in sys.argv:
+            return
+
         from cmms.timer import Timer
 
         if "django.core.wsgi" in sys.modules or "django.core.asgi" in sys.modules:
