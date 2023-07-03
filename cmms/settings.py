595ca4bb1db5d6511b8548eaa82ab70cd315c31e
ziro <palembani@gmail.com>
2023-07-03 09:09:19 +0700

    fix(settings): Allow ngrok

--- cmms/settings.py
+++ cmms/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = getenv(
 if not SECRET_KEY and not DEBUG:
     raise RuntimeError("You must set DJANGO_SECRET_KEY in production mode!")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".ngrok-free.app"]
 
 
 # Application definition
